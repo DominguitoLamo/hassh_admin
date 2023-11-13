3cb87e6ea524dc75ffa603349db32d964a1b8d1b
chonlam <1685020647@qq.com>
2023-11-13 09:41:20 +0800

    optimze: shorten waiting time for group task

--- src/components/GroupTask.vue
+++ src/components/GroupTask.vue
@@ -66,7 +66,7 @@ async function runGroupTasks(record) {
             clearInterval(downloadInfo.intervalKey)
             downloadGroupFile(downloadInfo.key)
         }
-    }, 3000 * record.tasks.length)
+    }, 5000)
 }
 
 getGroupInfosData()
